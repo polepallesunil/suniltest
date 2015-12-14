21c94d1222a688920871007fad649a28209e4a32
Sunil Polepalle <vpolepal@akamai.com>
2015-12-14 19:50:52 +0530

    sass project

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
-var requireDir = require('require-dir');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
+{
+  "name": "Suni",
+  "version": "0.0.1",
+  "description": "",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/polepallesunil/suniltest.git"
+  },
+  "author": "Sunil Polepalle",
+  "devDependencies": {
+    "gulp": "^3.8.11",
+    "gulp-jshint": "^1.9.0",
+    "gulp-ruby-sass": "^1.0.5",
+    "gulp-sourcemaps": "^1.5.2",
+    "gulp-webserver": "^0.9.1"
+  }
+}
